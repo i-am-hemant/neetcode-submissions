a0db954a0e8423135c458454fbf1551c70ac2a39
Hemant Faujdar <hemantmoroli19@gmail.com>
2026-09-01 19:49:26 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int> rightMax(n, 0);
+        rightMax[n-1]=prices[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightMax[i]=max(rightMax[i+1], prices[i]);
+        }
+
+        int profit=0;
+        for(int i=0;i<n;i++){
+            profit=max(profit, rightMax[i]-prices[i]);
+        }
+        return profit;
+    }
+};
